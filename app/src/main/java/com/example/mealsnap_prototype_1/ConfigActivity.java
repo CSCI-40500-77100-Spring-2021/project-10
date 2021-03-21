f8bc02717c8d3ccbabec9a2a9a845358c5ad5ca2
svhnstorage <svhnstorage@gmail.com>
2021-03-21 10:32:04 -0400

    Ack old pr and added API Req w/ get

--- app/src/main/java/com/example/mealsnap_prototype_1/ConfigActivity.java
+++ app/src/main/java/com/example/mealsnap_prototype_1/ConfigActivity.java
@@ -4,10 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
+import java.io.IOException;
+
+import okhttp3.ResponseBody;
+import services.authhandler.APIRequest;
+import services.authhandler.APIServiceResponseEvent;
 import services.authhandler.Amplify;
 
 public class ConfigActivity extends AppCompatActivity {
+
+    public static final String TAG = "ConfigActivity";
+    public static String ROOT_URL = "https://dbkw974ay1.execute-api.us-east-1.amazonaws.com/";
+
+    private TextView debugin;
 
     //Config activity will config stuff we need to(passes or succeeds activity just redirects with intents to what we wanna check).
     @Override
@@ -15,17 +26,42 @@ public class ConfigActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_config);
 
-        //Test Amplify Config
-        Amplify testAmplify = new Amplify();
-        testAmplify.configure(getApplicationContext());
+        debugin = findViewById(R.id.debugin);
 
+        //Test Amplify Config
+        try {
+            Amplify.configure(getApplicationContext());
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+
+        //Test API Get Request
+        APIRequest.get("echo", new APIServiceResponseEvent() {
+            @Override
+            public void onSuccess(ResponseBody requestBody) {
+
+                    ConfigActivity.this.runOnUiThread(() -> {
+                        try {
+                            debugin.setText(requestBody.string());
+
+                        } catch (IOException ioException) {
+                            ioException.printStackTrace();
+                        }
+                    });
+            }
+
+            @Override
+            public void onFailure(IOException ioException) {
+                ioException.printStackTrace();
+            }
+        });
 
         //Redirect
-        goToSignupActivity();
+        loadMainAppActivity();
     }
 
-    //moves to Signup
-    private void goToSignupActivity() {
+    //moves to loadMainAppActivity
+    private void loadMainAppActivity() {
         Intent i = new Intent(this, SignupActivity.class);
         startActivity(i);
         finish();
--- app/src/main/java/services/authhandler/Amplify.java
+++ app/src/main/java/services/authhandler/Amplify.java
@@ -6,8 +6,10 @@ import android.util.Log;
 import com.amplifyframework.AmplifyException;
 import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin;
 
+import java.io.IOException;
+
 public class Amplify {
-    public static void configure(Context context) {
+    public static void configure(Context context) throws IOException {
         try {
             com.amplifyframework.core.Amplify.addPlugin(new AWSCognitoAuthPlugin());
             com.amplifyframework.core.Amplify.configure(context);
