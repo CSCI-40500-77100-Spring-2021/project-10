7374e9319c269d7548185b69603e86d6949f5807
svhnstorage <svhnstorage@gmail.com>
2021-04-01 21:01:15 -0400

    all but detail activity

--- app/src/main/java/com/example/mealsnap_prototype_2/MainActivity.java
+++ app/src/main/java/com/example/mealsnap_prototype_2/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.mealsnap_prototype_2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView btmHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btmHome = findViewById(R.id.btmHome);
+
+
     }
 }
