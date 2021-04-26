ad20b40d82463e41b000fce652a530bdce75b3bc
Asif Shikder <shikdernyc@gmail.com>
2021-04-26 15:46:36 -0400

    Added Page Response Object

---
+++ app/src/main/java/services/authhandler/APIRequestPageResponse.java
@@ -0,0 +1,14 @@
+package services.authhandler;
+
+import androidx.annotation.Nullable;
+
+// Reusable response data class for requests that returns page response
+// Example Response: https://www.notion.so/API-Documentation-c622f4f78c7941c987a4362acb73b6cc#f1ebaa442a3d4f0a81291cba55cb6a10
+public class APIRequestPageResponse {
+    String next;
+
+    @Nullable
+    public String getNext() {
+        return next;
+    }
+}
